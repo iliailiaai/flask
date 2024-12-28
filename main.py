b2ebaae729ac3ab4f12251c234c22aef56930cd1
iliailiaai <131950015+iliailiaai@users.noreply.github.com>
2024-12-28 14:51:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask
+import flask
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 
 @app.route('/')
 def index():
-  return Hello!
+    return "Hello World!"
 
 if __name__ == '__main__':
-  app.run(port=5000)
+    app.run()
