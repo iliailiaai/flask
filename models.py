e88952a9b48a40f419fc60838ef9c50d1d775203
iliailiaai <131950015+iliailiaai@users.noreply.github.com>
2025-06-17 18:50:01 +0300

    Update models.py

--- models.py
+++ models.py
@@ -14,16 +14,15 @@ class MultiplyRecord(db.Model):
 
 
 
-"""
 class User(db.Model):
     __tablename__ = 'users'
-    id_email = db.Column(db.String, primary_key=True)
+    id_email = db.Column(db.String(100), primary_key=True)
     user_data = db.relationship("UserData", back_populates="user", uselist=False, cascade="all, delete-orphan")
 
 class UserData(db.Model):
     __tablename__ = 'user_data'
     id = db.Column(db.Integer, primary_key=True)
-    user_email = db.Column(db.String, db.ForeignKey('users.id_email'), nullable=False, unique=True)
+    user_email = db.Column(db.String(100), db.ForeignKey('users.id_email'), nullable=False, unique=True)
 
     purpose = db.Column(db.String(100))
     gender = db.Column(db.String(20))
@@ -34,4 +33,4 @@ class UserData(db.Model):
     age = db.Column(db.Integer)
 
     user = db.relationship("User", back_populates="user_data")
-"""
+
