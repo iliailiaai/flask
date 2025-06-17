c54a4f48fa6b1288be96bd6b9da3e5e833e83156
iliailiaai <131950015+iliailiaai@users.noreply.github.com>
2025-06-17 17:46:15 +0300

    Create models.py

---
+++ models.py
@@ -0,0 +1,12 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class MultiplyRecord(db.Model):
+    __tablename__ = 'multiply_records'
+    id     = db.Column(db.Integer, primary_key=True)
+    input  = db.Column(db.Integer, nullable=False)
+    result = db.Column(db.Integer, nullable=False)
+
+    def to_dict(self):
+        return {"id": self.id, "input": int(self.input), "result": int(self.result)}
