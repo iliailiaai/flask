c1d6ddae0d179d3a1113062f28f8370ed643ddcf
iliailiaai <131950015+iliailiaai@users.noreply.github.com>
2025-06-22 23:01:26 +0300

    Update models.py

--- models.py
+++ models.py
@@ -42,7 +42,7 @@ class CompletedDates(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_email = db.Column(db.String(100), db.ForeignKey('users.id_email'), nullable=False)
 
-    date = db.Column(db.Integer)
+    date_ = db.Column(db.Integer)
 
 
 class ProgramModel(db.Model):
